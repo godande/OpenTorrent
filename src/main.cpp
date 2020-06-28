cedf45c071dfda501db64f0a7f76e6be02e96f9e
dande <dande@lads.aletheia.icu>
2020-06-29 02:32:59 +0300

    fixed runtime

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@
 #include "logger.h"
 #include "torrentmultiplefileInfo.h"
 #include "torrentsinglefileinfo.h"
-
+#include "AnnouncePacket.h"
 int main() {
   Logger::get_instance()->Debug("KEK");
-  std::ifstream input_file{
-      "../test-torrent-files/doctor-who-single-file.torrent"};
+  std::ifstream input_file(
+      "/home/prise/CLionProjects/CockTorrent/test-torrent-files/"
+      "doctor-who-single-file.torrent",
+      std::ios::binary | std::ifstream::in);
+  std::cout << input_file.is_open() << '\n';
   std::string expression{std::istreambuf_iterator<char>{input_file},
                          std::istreambuf_iterator<char>{}};
   auto res = bencode::Decode(expression);
@@ -18,8 +21,10 @@ int main() {
   std::cout << s_file_s_info.announce() << '\n';
 
   input_file.close();
-  input_file.open("../test-torrent-files/Gravity-Falls-Season-1.torrent",
-                  std::ios_base::in);
+  input_file.open(
+      "/home/prise/CLionProjects/CockTorrent/test-torrent-files/"
+      "Gravity-Falls-Season-1.torrent",
+      std::ios::binary | std::ios_base::in);
   expression = std::string{std::istreambuf_iterator<char>{input_file},
                            std::istreambuf_iterator<char>{}};
   res = bencode::Decode(expression);
