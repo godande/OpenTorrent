ab03da3196df256763d0ba8d6e94882beaeb6c55
Shlyaga Vyacheslav <slava.shlyaga@mail.ru>
2020-07-27 12:46:11 +0300

    f

---
+++ src/include/peertransfer.h
@@ -0,0 +1,16 @@
+//
+// Created by Linux Oid on 27.07.2020.
+//
+
+#ifndef COCKTORRENT_PEERTRANSFER_H
+#define COCKTORRENT_PEERTRANSFER_H
+
+namespace cocktorrent::peer::tcp {
+class PeerTransfer {
+ public:
+
+ private:
+};
+}  // namespace cocktorrent::peer::tcp
+
+#endif  // COCKTORRENT_PEERTRANSFER_H
---
+++ src/peertransfer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Linux Oid on 27.07.2020.
+//
+
+#include <peertransfer.h>
